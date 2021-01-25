06e901227ad57d83e75fde5b0783e3e2a76756c2
natmotykova <76699972+natmotykova@users.noreply.github.com>
2021-01-25 20:54:42 +0100

    Upload data for Czechia

---
+++ CZ/generate_plots.py
@@ -0,0 +1,58 @@
+# create plots
+
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+# dictionary for conversion of variables (velicina)
+prevodnik = {
+            'kumulativni_pocet_nakazenych': 'Kumulativní počet nakažených',
+            'kumulativni_pocet_vylecenych': 'Kumulatnvní počet vyléčených',
+            'kumulativni_pocet_umrti': 'Kumulativní počet úmrtí',
+            'kumulativni_pocet_testu': 'Kumulativní počet testů',}
+
+
+def create_plot(x,y, velicina, date_modified):
+    '''
+    x - vstupni data pro osu x (datum)
+    y - vstupni data pro osu y
+    velicina - typ grafu (lisi se podle veliciny/promenne)
+    '''
+
+    # vytvoreni noveho okna pro graf
+    plt.figure(figsize=(10,6))
+
+    # reformat date
+    x_reformated = list()
+    for datum in x:
+        x_reformated.append(datetime.strptime(datum,"%Y-%m-%d").strftime("%d.%m.%Y"))  
+
+    # plotting the points
+    plt.plot(x_reformated, y, linestyle='dashed', color='r')
+
+    # change xticks to weeks
+    # data co 14 dni; vytvari list skrze cyklus
+    plt.xticks(range(0,len(x_reformated),14), [x_reformated[w] for w in range(0,len(x_reformated),14)], rotation='90', fontsize = 10)
+
+    # namig the x axis
+    plt.xlabel('datum [14 dní]', fontsize = 12)
+
+    # naming the y axis
+    plt.ylabel(prevodnik[velicina], fontsize = 12)
+
+    # giving a title to my graph
+    plt.title('Vývoj epidemie koronaviru v ČR', fontsize = 16)
+
+    # fit everything to figure
+    plt.tight_layout()
+
+    # nastaveni mrizky (od urciteho data)
+    plt.xlim([x_reformated[0],x_reformated[-1]])
+
+    #grid
+    plt.grid(True)
+
+    # saving plots
+    plot_name = velicina + '_chart_' + date_modified + '.png'
+    plt.savefig(plot_name, dpi=200)
+
+    print(f'Succesfully created {plot_name}')
---
+++ CZ/process_data.py
@@ -0,0 +1,62 @@
+# data only for Czechia
+
+import requests
+import json
+import matplotlib.pyplot as plt
+from datetime import datetime
+from generate_plots import create_plot
+
+
+# seznam odkazu (kumulativni pocet nakazenych = celkovy pocet nakazenych)
+list_of_urls = ['https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/nakazeni-vyleceni-umrti-testy.json',
+                'https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/testy.json',
+                'https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/nakaza.json',
+                'https://onemocneni-aktualne.mzcr.cz/api/v1/covid-19/nakazeni-vyleceni-umrti-testy.json']
+
+datum_start = '2020-03-01'      # pocatecni datum v grafu
+
+
+def get_data(url):
+    '''
+    Funkce pro nacteni url a konverzi do jsonu
+    '''
+    data = requests.get(url)   # data Mz
+    data.raise_for_status()
+
+    print(f'Succesfully uploaded data from {url}')  # pro kontrolu nacteni dat
+
+    # Konverze z textoveho JSON formatu na Pythoni objekt
+    json_data = json.loads(data.text)
+
+    return json_data
+
+
+json_data = get_data(list_of_urls[0])   # pozice odkazu
+
+
+# datum aktualizace json souoru na webu
+date_modified = json_data['modified']
+
+
+seznam_datum = list()
+seznam_kumulativni_pocet = list()
+seznam_kumulativni_pocet_vylecenych = list()
+seznam_kumulativni_pocet_umrti = list()
+seznam_kumulativni_pocet_testu = list ()
+
+for radek in json_data['data']:
+    seznam_kumulativni_pocet.append(radek['kumulativni_pocet_nakazenych'])
+    seznam_kumulativni_pocet_vylecenych.append(radek['kumulativni_pocet_vylecenych'])
+    seznam_kumulativni_pocet_umrti.append(radek['kumulativni_pocet_umrti'])
+    seznam_kumulativni_pocet_testu.append(radek['kumulativni_pocet_testu'])
+    seznam_datum.append(radek['datum'])
+
+
+# starting date
+index_datum = seznam_datum.index(datum_start)
+
+
+create_plot(seznam_datum[index_datum:], seznam_kumulativni_pocet[index_datum:], 'kumulativni_pocet_nakazenych', date_modified)
+create_plot(seznam_datum[index_datum:], seznam_kumulativni_pocet_vylecenych[index_datum:], 'kumulativni_pocet_vylecenych', date_modified)
+create_plot(seznam_datum[index_datum:], seznam_kumulativni_pocet_umrti[index_datum:], 'kumulativni_pocet_umrti', date_modified)
+create_plot(seznam_datum[index_datum:], seznam_kumulativni_pocet_testu[index_datum:], 'kumulativni_pocet_testu', date_modified)
